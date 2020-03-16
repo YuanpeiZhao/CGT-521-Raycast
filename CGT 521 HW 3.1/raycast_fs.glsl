442407bf6a1cfa7664252fd1a0a2ba86e40db7f9
Yuanpei Zhao <yuanpeizhao1106@gmail.com>
2020-03-16 14:45:02 -0400

    Finished

--- CGT 521 HW 3.1/raycast_fs.glsl
+++ CGT 521 HW 3.1/raycast_fs.glsl
@@ -29,6 +29,7 @@ const vec4 Ka = vec4(0.4, 0.4, 0.34, 1.0);
 const vec4 Kd = vec4(1.0, 1.0, 0.73, 1.0);
 const vec4 Ks = vec4(0.1, 0.1, 0.073, 1.0);
 
+float calcSoftshadow( in vec3 ro, in vec3 rd, in float mint, in float tmax, int technique );
 
 void main(void)
 {   
@@ -95,21 +96,31 @@ vec4 lighting(vec3 pos, vec3 rayDir)
 	vec3 n = normal(pos);
 
 	vec4 La = clear_color(n);
-	float diff = max(0.0, dot(n, light));
+	float diff = max(0.0, dot(n, light)) * calcSoftshadow( pos, light, 0.01, 3.0, 1 );             
 
 	return La*Ka + Ld*Kd*diff;	
 }
 
 vec4 clear_color(vec3 rayDir)
 {
-	const vec4 color1 = vec4(0.6, 0.8, 0.9, 1.0);
-	return color1;
+	const vec4 color1 = vec4(1.0, 0.7, 0.8, 1.0);
+	const vec4 color2 = vec4(0.8, 0.8, 0.9, 1.0);
+	return mix(color1, color2, sin(10 * dot(rayDir, vec3(1.0f, 0.0f, 0.0f))));
 }
 
 
 //shape function declarations
 float sdSphere( vec3 p, float s );
 float sdBox( vec3 p, vec3 b );
+float sdCapsule( vec3 p, vec3 a, vec3 b, float r );
+float sdRoundedCylinder( vec3 p, float ra, float rb, float h );
+float sdTorus( vec3 p, vec2 t );
+float udQuad( vec3 p, vec3 a, vec3 b, vec3 c, vec3 d );
+
+float opU( float d1, float d2 )
+{
+	return (d1<d2) ? d1 : d2;
+}
 
 // For more distance functions see
 // http://iquilezles.org/www/articles/distfunctions/distfunctions.htm
@@ -127,23 +138,28 @@ float distToShape(vec3 pos)
 	if(scene == 0)
 	{
 		const float radius = 0.4;
-		vec3 offset = 2.0*slider.xyz;
-		float d0 = sdSphere(pos+offset, radius);
+		//vec3 offset = 2.0*slider.xyz;
+		//float d0 = sdSphere(pos+offset, radius);
+		float d0 = sdCapsule(pos, vec3(radius*sin(time), radius*cos(time), radius), vec3(-radius*sin(time), -radius*cos(time), -radius), 0.2f );
 		return d0;
 	}
 
 	else if(scene == 1)
 	{
-		const float radius = 0.4;
-		vec3 c = vec3(1.0, 1.0, 1.0) + 2.0*slider.xyz;
-		vec3 q = mod(pos,c)-0.5*c;
-		float d1 = sdSphere(q, radius);
+		//const float radius = 0.4;
+		//vec3 c = vec3(1.0, 1.0, 1.0) + 2.0*slider.xyz;
+		//vec3 q = mod(pos,c)-0.5*c;
+		//float d1 = sdSphere(q, radius);
+		float d1 = sdRoundedCylinder(pos + 0.5 * slider.x, 0.1f + 0.05 * slider.y, 0.03f + 0.03 * slider.z, 0.2f + 0.1 * slider.w );
 		return d1;
 	}
 
 	else if(scene == 2)
 	{
-		float d2 = sdBox(pos+vec3(0.0, 0.0, 0.0), vec3(0.4, 0.4, 0.4));
+		const float len = 0.7;
+		//float d2 = sdBox(pos+vec3(0.0, 0.0, 0.0), vec3(0.4, 0.4, 0.4));
+		float d2 = sdTorus(pos + vec3(0.0, -0.4, -0.1), vec2(0.5, 0.1));
+		d2 = opU(d2, udQuad( pos, vec3(len, len, -0.5), vec3(len, -len, -0.5), vec3(-len, -len, -0.5), vec3(-len, len, -0.5) ));
 		return d2;
 	}
 }
@@ -158,6 +174,85 @@ float sdBox( vec3 p, vec3 b )
 {
   vec3 d = abs(p) - b;
   return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
+}
+
+float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+{
+  vec3 pa = p - a, ba = b - a;
+  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+  return length( pa - ba*h ) - r;
+}
+
+float sdRoundedCylinder( vec3 p, float ra, float rb, float h )
+{
+  vec2 d = vec2( length(p.xz)-2.0*ra+rb, abs(p.y) - h );
+  return min(max(d.x,d.y),0.0) + length(max(d,0.0)) - rb;
+}
+
+float sdTorus( vec3 p, vec2 t )
+{
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
+}
+
+float dot2( vec3 v ) { return dot(v,v); }
+float udQuad( vec3 p, vec3 a, vec3 b, vec3 c, vec3 d )
+{
+  vec3 ba = b - a; vec3 pa = p - a;
+  vec3 cb = c - b; vec3 pb = p - b;
+  vec3 dc = d - c; vec3 pc = p - c;
+  vec3 ad = a - d; vec3 pd = p - d;
+  vec3 nor = cross( ba, ad );
+
+  return sqrt(
+    (sign(dot(cross(ba,nor),pa)) +
+     sign(dot(cross(cb,nor),pb)) +
+     sign(dot(cross(dc,nor),pc)) +
+     sign(dot(cross(ad,nor),pd))<3.0)
+     ?
+     min( min( min(
+     dot2(ba*clamp(dot(ba,pa)/dot2(ba),0.0,1.0)-pa),
+     dot2(cb*clamp(dot(cb,pb)/dot2(cb),0.0,1.0)-pb) ),
+     dot2(dc*clamp(dot(dc,pc)/dot2(dc),0.0,1.0)-pc) ),
+     dot2(ad*clamp(dot(ad,pd)/dot2(ad),0.0,1.0)-pd) )
+     :
+     dot(nor,pa)*dot(nor,pa)/dot2(nor) );
+}
+
+float calcSoftshadow( in vec3 ro, in vec3 rd, in float mint, in float tmax, int technique )
+{
+	float res = 1.0;
+    float t = mint;
+    float ph = 1e10; // big, such that y = 0 on the first iteration
+    
+    for( int i=0; i<32; i++ )
+    {
+		float h = distToShape( ro + rd*t );
+
+        // traditional technique
+        if( technique==0 )
+        {
+        	res = min( res, 10.0*h/t );
+        }
+        // improved technique
+        else
+        {
+            // use this if you are getting artifact on the first iteration, or unroll the
+            // first iteration out of the loop
+            //float y = (i==0) ? 0.0 : h*h/(2.0*ph); 
+
+            float y = h*h/(2.0*ph);
+            float d = sqrt(h*h-y*y);
+            res = min( res, 10.0*d/max(0.0,t-y) );
+            ph = h;
+        }
+        
+        t += h;
+        
+        if( res<0.0001 || t>tmax ) break;
+        
+    }
+    return clamp( res, 0.0, 1.0 );
 }
 
 //normal vector of the shape we are drawing.
